4d40ed5beb059b620ea10ba68f51c87f8b22ef41
donatKapesa <donatkap14@gmail.com>
2018-09-21 20:46:40 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "Obstacle") {
+            // game over or reduce life
+            // communicate with PlayerMovement and turn on  gravity
+            // I want player movement to stop completely
+            // maybe use constraints
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 250;
+    public float sideForce = 200;
+     
+
+    /*
+     * Two problems:
+     * Input.GetKey is not very good e.g. if you using a Tablet
+     * Player movement should be inside Update and not Fixed Update
+     */
+
+	void FixedUpdate () {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // because framerate differs on diff computers
+
+        if(Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow)) {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1f) {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+	}
+}
